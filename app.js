cfd6da0c2456e4ce407957a4efe6b359915cd2c6
Charles Lee <chug2k@Product-Market-Fit.local>
2021-03-03 13:48:06 +0700

    Updating Mongo DB URI

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ var express = require("express");
 var logger = require("morgan");
 const mongoose = require("mongoose");
 const cors = require("cors");
+require("dotenv").config();
 
 var indexRouter = require("./routes/index");
 var experiencesRouter = require("./routes/experiences");
 
 var app = express();
-mongoose.connect("mongodb://localhost/airbnb-2");
+console.log("Mongo URI: " + process.env.MONGO_DB_URI);
+mongoose.connect(process.env.MONGO_DB_URI);
 
 app.use(logger("dev"));
 app.use(express.json());
